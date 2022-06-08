0997bb804f8c5bda096da6c347cbb554a4ed068a
richarddwipu <richard9dwiputra@gmail.com>
2022-06-08 13:07:23 +0700

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -1188,5 +1188,4 @@
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
   </body>
-</html>
 richard
